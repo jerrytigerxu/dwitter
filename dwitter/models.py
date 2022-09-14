c0a1c6356c7a1a33532215edd4fe1e1458ee3b35
jerrytigerxu <jere.tiger.xu@gmailc.om>
2022-09-14 14:47:32 -0400

    Initialized

---
+++ dwitter/models.py
@@ -0,0 +1,48 @@
+# dwitter/models.py
+
+from django.db import models
+from django.contrib.auth.models import User
+# from django.db.models.signals import post_save
+from django.dispatch import receiver
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        symmetrical=False,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
+
+class Dweet(models.Model):
+    user = models.ForeignKey(
+        User, related_name="dweets", on_delete=models.DO_NOTHING
+    )
+    body = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"{self.user} "
+            f"({self.created_at:%Y-%m-%d %H:%M}): "
+            f"{self.body[:30]}..."
+        )
+
+
+
+# Get these weird errors involving IntegrityError (Unique constraint dwitter_profile.user_id) 
+# even though my code was exactly the same as that of the tutorial, so I felt that it wasn't worth the 
+# time to deal with it when the problem we were trying to fix wasn't even that big of a deal
+"""@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.add(instance.profile)
+        user_profile.save()"""
+
+
